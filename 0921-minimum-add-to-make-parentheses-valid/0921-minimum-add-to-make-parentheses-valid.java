9ffeb810ce6fe0b336275dfa8347b489c079833d
Ayushi Varshney <121843249+varshney-23@users.noreply.github.com>
2024-10-10 10:07:24 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (28.61%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open= 0, close=0;
+        for(char c: s.toCharArray()){
+            if(c=='(') open++;
+            else if (c==')' && open>0) open--;
+            else close++;
+        }
+        return open+close;
+    }
+}
